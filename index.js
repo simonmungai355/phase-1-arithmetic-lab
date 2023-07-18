552d11644d51792c762d378e522dfd7570dbdae5
simonmungai355 <simon.mungai1@student.moringaschool.com>
2023-07-18 15:57:10 +0300

    assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+var multiply = num1 * num2
+{
+    let num1 =31
+    let num2 = 2
+console.log (multiply)
+}
 
-// Write your code here
+Math.random(Math.random() * 10) + 1;
+{
+    console.log (Math.random)
+
+}
+const mod = (num3 % num4)
+{let num3 = 172
+let num4 = 42
+console.log (mod)
+} 
+
+Math.max([10,9,18,20,11])
+return (Math.max)
+/* Write your code here
